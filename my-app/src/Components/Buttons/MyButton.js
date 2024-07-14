0fb4d1911220877cf0cdd518de82ec2060d730c5
shanesreal <shanesreal@gmail.com>
2024-07-14 19:28:01 -0400

    Update button component to accept route param

--- my-app/src/Components/Buttons/MyButton.js
+++ my-app/src/Components/Buttons/MyButton.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
 
-const MyButton = ({ name, style }) => {
+const MyButton = ({ name, style, route }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    if (route) {
+      navigate(route);
+    }
+  };
+
   return (
-    <Button variant="contained" style={style}>
+    <Button variant="contained" style={style} onClick={handleClick}>
       {name}
     </Button>
   );

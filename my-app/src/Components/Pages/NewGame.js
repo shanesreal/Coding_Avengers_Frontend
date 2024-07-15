a208a354e272ac5346e0722de946c956bdd4162b
Andrew Nixon <andjosnixon@gmail.com>
2024-07-15 19:02:43 -0400

    adding to NewGame

--- my-app/src/Components/Pages/NewGame.js
+++ my-app/src/Components/Pages/NewGame.js
@@ -1,5 +1,37 @@
+
+import { Box, Grid, Typography } from "@mui/material"
+import MyButton from "../Buttons/MyButton"
+
 export const NewGame = () => {
-    return ('new game page')
+    return (
+        <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="100vh"
+      >
+        <Typography variant="h4" align="center" gutterBottom>
+          Choose your name
+        </Typography>
+        <Grid container spacing={2} justifyContent="center">
+          <Grid item xs={8}>
+            <MyButton name="Player one" style={{ background: 'white', color: 'black' }} />
+          </Grid>
+          <Grid item xs={4}>
+            <MyButton name="Player two" style={{  background: 'white', color: 'black' }} />
+          </Grid>
+        </Grid>
+        <Grid container spacing={2} justifyContent="center">
+          <Grid item xs={4}>
+            <MyButton name="Player three" style={{  background: 'white', color: 'black' }} />
+          </Grid>
+          <Grid item xs={8}>
+            <MyButton name="Player four" style={{  background: 'white', color: 'black' }} />
+          </Grid>
+        </Grid>
+    </Box>
+    )
 }
 
 export default NewGame;
